82ea11b525d049a6f5f28db072fd7e9a7ec72045
benwang2 <eb.benwang@gmail.com>
2022-01-05 16:20:49 -0500

    Uploaded files.

--- script.js
+++ script.js
@@ -1,96 +1,37 @@
-let projects = [
-    ["#BD85FF","resources/img/lotus.jpg","https://github.com/benwang2/Lotus",
-    "Lotus", " A service that tracks user activity and facilitates organizing events within small communities."],
-    ["#0F2BA5", "resources/img/psg.jpg","https://github.com/benwang2/ProblemSetGenerator",
-    "Problem Set Generator","A website to quickly create unique problem sets based on existing ones."    ],
-    ["#13305a","resources/img/stegafun.jpg","https://benwang2.github.io/stega.fun/",
-    "stegafun","Web steganography tools inspired by a previous project written in Java."],
-    ["#14a76c","resources/img/mnky.jpg","https://mnky.link",
-    "mnky.link","A comical URL shortener that uses monkey-related emojis to shorten your URL."],
-    ["#640f0e","resources/img/betterburn.jpg","https://betterburn.xyz",
-    "betterburn.xyz","My first website, created to display my Rivals of Aether-related projects."],
-]
+window.addEventListener("load",(e)=>{
+    let nav = document.getElementById("nav")
+    let nav_container = nav.querySelector("div")
+    let nav_desktop = document.getElementById("navbar-desktop")
+    let nav_mobile = document.getElementById("navbar-mobile")
+    let nav_menu_button = document.getElementById("navbar-mobile-button")
 
-let sections = [];
-let navbars = []
+    let anchors = [document.getElementsByClassName("text-center")[0],document.getElementById("about")]
 
-function addClass(name) {
-    var element, name, arr;
-    element = document.getElementById("full");
-    arr = element.className.split(" ");
-    if (arr.indexOf(name) == -1) {
-        element.className += " " + name;
-    }
-}
-
-function rmvClass(name){
-    var element, name, arr;
-    element = document.getElementById("full");
-    arr = element.className.split(" ");
-    if (arr.indexOf(name) >= 0) {
-        arr.splice(arr.indexOf(name), 1)
-        element.className = arr.join(" ")
-    }   
-}
-
-function updateMenu(){
-    if (window.innerWidth < 500){
-        navbars[0].style = "display:none;"
-        navbars[1].style = ""
-    } else {
-        navbars[0].style = ""
-        navbars[1].style = "display:none;"
-    }
-}
-
-function toggleMenu(){
-    //console.log("hello"+hamburger.classList.contains("is-active"))
-    if (hamburger.classList.contains("is-active")){
-        hamburger.classList.remove("is-active")
-        dropdown.classList.remove("visible")
-    } else {
-        hamburger.classList.add("is-active")
-        dropdown.classList.add("visible")
-    }
-}
-
-function hideMenu(){
-    if (hamburger.classList.contains("is-active")){
-        hamburger.classList.remove("is-active")
-        dropdown.classList.remove("visible")
-    }
-}
-
-window.addEventListener("load",()=>{
-    hamburger = document.getElementById("hamburger")
-    dropdown = document.getElementsByClassName("dropdown")[0]
-    navbars[0] = document.getElementById("full")
-    navbars[1] = document.getElementById("compressed")
-
-    sections = [document.getElementById("home"),document.getElementById("about"),document.getElementById("menu"),document.getElementById("contact")];
-
-    let cell = document.getElementsByClassName("cell")[0]
-    let grid = document.getElementById("project-grid")
-
-    for (let x = 0; x <  projects.length; x++){
-        let newCell = cell.cloneNode(true);
-        newCell.style = "border-color:"+projects[x][0]+";background-image:url('"+projects[x][1]+"')";
-        newCell.href = projects[x][2]
-        newCell.getElementsByTagName("h1")[0].innerHTML = projects[x][3]
-        newCell.getElementsByTagName("p")[0].innerHTML = projects[x][4]
-        grid.appendChild(newCell)
+    function updateNav(e){
+        nav_desktop.classList.toggle("hidden", window.innerWidth < 824)
+        nav_mobile.classList.toggle("hidden", window.innerWidth >= 824)
+        nav.classList.toggle("nav-opaque",(window.scrollY > (anchors[0].offsetTop-12) || nav_desktop.classList.contains("hidden")))
+        nav_container.classList.toggle("row", !(nav_desktop.classList.contains("hidden")))
     }
 
-    hamburger.addEventListener("click",toggleMenu)
-    updateMenu()
-    window.addEventListener('resize', updateMenu);
-})
-
-window.addEventListener("scroll",(evt)=>{
-    let scrollPosition = window.scrollY;
-    if (scrollPosition <= (sections[1].offsetTop - 61)){
-        addClass("opaque-black")
-    } else {
-        rmvClass("opaque-black")
+    function toggleNav(e){
+        nav_menu_button.classList.toggle("is-active")
+        nav_mobile.getElementsByTagName("ul")[0].classList.toggle("hidden", !nav_menu_button.classList.contains("is-active"))
     }
+
+    function setNav(e,val){
+        nav_menu_button.classList.toggle("is-active",val)
+        nav_mobile.getElementsByTagName("ul")[0].classList.toggle("hidden", !nav_menu_button.classList.contains("is-active"))
+    }
+
+    nav_menu_button.addEventListener("click",toggleNav)
+    window.addEventListener("scroll", updateNav)
+    window.addEventListener("resize", updateNav)
+    updateNav()
+
+    let links = [...nav_container.getElementsByTagName("li")]
+    for (let i = 0; i < links.length; i++){
+        links[i].addEventListener("click",(e)=>{setNav(e,false)})
+    }
+
 })
